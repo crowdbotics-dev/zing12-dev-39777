2c3f7640c0ec238311fc4e284b496658641c5cb1
Crowdbotics <engineering@crowdbotics.com>
2022-10-20 17:40:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/zing12_dev_39777/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zing12_dev_39777;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "zing12_dev_39777";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "zing12_dev_39777",
+  url: "https://zing12_dev_39777.botics.co",
+  api: "https://zing12_dev_39777.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
